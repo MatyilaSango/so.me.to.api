82ddd3af5f90f0a0148a0c03ba2b0c2a167091c4
Sango <matyila2sango@gmail.com>
2024-07-17 21:52:18 +0200

    Add auth microservice

--- src/gate-way/modules/app/app.module.ts
+++ src/gate-way/modules/app/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH',
+        transport: Transport.TCP,
+        options: {
+          host: 'localhost',
+          port: 3001,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
