a064d615208a3ac83a25a4b30264dd3559fd70d6
Sango <matyila2sango@gmail.com>
2024-07-21 00:08:04 +0200

    Chore: Add docker

--- gateway/src/modules/auth/auth.module.ts
+++ gateway/src/modules/auth/auth.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_MICROSERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'auth-queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
