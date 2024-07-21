1e681e39f58d2fc699dbe7a6a766c7c4937f2b46
Sango <matyila2sango@gmail.com>
2024-07-21 16:54:04 +0200

    Update bash file

--- bash.sh
+++ bash.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-echo "------- Starting operations --------"
-
-# Gateway service operation
-
-gateway=gateway
-auth=auth
-
-echo Moving to $gateway directory
-
-cd gateway
-
-docker build -t gateway .
-
-echo Finished building image $gateway
-
-# Auth service operation
-
-cd ../microservices/auth
-
-echo Moving to $auth directory
-
-docker build -t auth .
-
-echo Finished building image $auth
-
 ## Docker compose
 
 docker-compose up
