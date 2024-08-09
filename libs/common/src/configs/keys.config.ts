e074539305b4798d87cea793b76fe7b1da85b20f
Sango <matyila2sango@gmail.com>
2024-08-09 21:17:36 +0200

    Add jwt keys in config

--- libs/common/src/configs/keys.config.ts
+++ libs/common/src/configs/keys.config.ts
@@ -1,8 +1,14 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('keys', () => ({
-  tokenSecrete: process.env.TOKEN_SECRETE,
-  tokenRefreshSecrete: process.env.TOKEN_REFRESH_SECRETE,
+  token: {
+    secrete: process.env.TOKEN_SECRETE,
+    Refresh: process.env.TOKEN_REFRESH_SECRETE,
+    expiry: {
+      access: process.env.JWT_ACCESS_EXPIRES,
+      refresh: process.env.JWT_REFRESH_EXPIRES,
+    },
+  },
   encryption: {
     key: process.env.ENCRYPTION_KEY,
     iv: process.env.ENCRYPTION_IV,
