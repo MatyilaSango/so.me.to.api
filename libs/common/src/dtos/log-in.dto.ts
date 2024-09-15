f3d4d9b76fb087042070e79240ebe285160e0b0b
Sango <matyila2sango@gmail.com>
2024-09-15 20:06:03 +0200

    Update login dto validation

--- libs/common/src/dtos/log-in.dto.ts
+++ libs/common/src/dtos/log-in.dto.ts
@@ -1,12 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateLogInDTO {
   @ApiProperty()
+  @MinLength(2)
   @IsNotEmpty()
   Username: string;
 
   @ApiProperty()
   @IsNotEmpty()
+  @MinLength(8)
   Password: string;
 }
