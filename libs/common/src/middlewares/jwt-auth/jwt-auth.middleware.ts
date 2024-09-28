079b7b4166aaebd751bb33bb8fcf704c4f06564a
Sango <matyila2sango@gmail.com>
2024-09-28 21:29:10 +0200

    Refactor: validate token existance and catch expired

--- libs/common/src/middlewares/jwt-auth/jwt-auth.middleware.ts
+++ libs/common/src/middlewares/jwt-auth/jwt-auth.middleware.ts
@@ -1,20 +1,25 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request, Response } from 'express';
 
 @Injectable()
 export class JwtAuthMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: () => void) {
-    const jwtService = new JwtService();
+  constructor(private jwtService: JwtService) {}
 
+  use(req: Request, res: Response, next: () => void) {
     const { authorization } = req.headers;
 
+    if (!authorization) throw new UnauthorizedException();
+
     const [, token] = authorization.split(' ');
-    const secret = process.env.TOKEN_SECRETE;
 
-    const user = jwtService.verify(token, { secret });
+    try {
+      const user = this.jwtService.verify(token);
 
-    req['user'] = user;
+      req['user'] = user;
+    } catch (tokenExpiredError) {
+      throw new UnauthorizedException('Token expired');
+    }
 
     next();
   }
