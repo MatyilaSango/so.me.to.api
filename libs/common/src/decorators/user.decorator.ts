c7e9eeaad9f87898ac0eb2fab43bd176a3042210
Sango <matyila2sango@gmail.com>
2024-09-28 19:18:27 +0200

    Chore: update userdoc decorator return type

--- libs/common/src/decorators/user.decorator.ts
+++ libs/common/src/decorators/user.decorator.ts
@@ -1,9 +1,10 @@
 import { createParamDecorator, ExecutionContext, UnprocessableEntityException } from '@nestjs/common';
+import { IAuthUser } from '../types/interfaces/auth.interface';
 
 /**
  * Custorm decorator to get request user object from request.
  */
-export const Userdoc = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+export const Userdoc = createParamDecorator((data: unknown, ctx: ExecutionContext): IAuthUser => {
   const request = ctx.switchToHttp().getRequest();
 
   const { user } = request;
